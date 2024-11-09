6d29f866bfef3a755154c5512201bf2392a0212a
Rafael Fernández <159128626+rfparra@users.noreply.github.com>
2024-11-09 11:38:05 +0100

    Update TMS_raton.py

--- TMS_raton.py
+++ TMS_raton.py
@@ -1,32 +1,28 @@
+""" How to run a SimNIBS TMS simulation in Python
+    Run with:
 
+    simnibs_python TMS.py
+    Copyright (C) 2018 Guilherme B Saturnino
+"""
 import os
 from simnibs import sim_struct, run_simnibs
-#print("paso1")
+
 ### General Information
 S = sim_struct.SESSION()
-#print("paso2")
 S.fnamehead = 'Mouse_Digimouse.msh'  # m2m-folder of the subject
-#print("paso3")
 S.pathfem = 'tms_simu'  # Directory for the simulation
-#print("paso4")
-S.fields = 'eE'
-#print("paso5")
+S.fields = 'eE' #Fields to calculate
+
 ## Define the TMS simulation
 tms = S.add_tmslist()
-#print("paso6")
 tms.fnamecoil = os.path.join('legacy_and_other','EstimuladorS05.tcd')  # Choose a coil model
-#print("paso7")
+
 # Define the coil position
 pos = tms.add_position()
-#print("paso8")
-pos.centre = [0.0,15.0,28.0]  # Place the coil over C3
-#print("paso9")
-pos.pos_ydir = [1.0,0.0,1.0]  # Polongation of coil handle (see documentation)
-#print("paso10")
-pos.distance = 4  #  4 mm distance from coil surface to head surface
-pos.didt = 6.283185307
-#print("paso11")
+pos.centre = [0.0,15.0,28.0] #Place the coil in a determinated position
+pos.pos_ydir = [1.0,0.0,1.0]  #Orientation of the coil
+pos.distance = 4  #Distance from coil surface to head surface (mm)
+pos.didt = 6.283185307 #Define the value of didt
 
 # Run Simulation
 run_simnibs(S)
-print("paso12")
