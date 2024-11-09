cf492d9a43232add483d31d25fac84a1daa21e43
Rafael Fernández <159128626+rfparra@users.noreply.github.com>
2024-11-09 11:35:19 +0100

    Update TMS.py

--- TMS.py
+++ TMS.py
@@ -1,20 +1,28 @@
+""" How to run a SimNIBS TMS simulation in Python
+    Run with:
+
+    simnibs_python TMS.py
+    Copyright (C) 2018 Guilherme B Saturnino
+"""
 import os
 from simnibs import sim_struct, run_simnibs
 
-
+###General information
 S = sim_struct.SESSION()
 S.subpath = 'm2m_ernie'  # m2m-folder of the subject
 S.pathfem = 'tms_simu'  # Directory for the simulation
-S.fields = 'eE'
+S.fields = 'eE' #Fields to calculate
+
 ## Define the TMS simulation
 tms = S.add_tmslist()
 tms.fnamecoil = os.path.join('legacy_and_other','ModeladoL1052.tcd')  # Choose a coil model
+
 # Define the coil position
 pos = tms.add_position()
-pos.centre = [0.0,15.0,99.0]  
-pos.pos_ydir = [1.0,0.0,1.0]  
-pos.distance = 4  #  4 mm distance from coil surface to head surface
-pos.didt = 6.283185307
+pos.centre = [0.0,15.0,99.0]  #Place the coil in a determinated position
+pos.pos_ydir = [1.0,0.0,1.0]  #Orientation of the coil
+pos.distance = 4  #Distance from coil surface to head surface (mm)
+pos.didt = 6.283185307 #Define de value of didt
 
 # Run Simulation
 run_simnibs(S)
